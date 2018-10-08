646025bed0da11145235106b2d4a69832e15a056
ErickVAleman <Erickva70@gmail.com>
2018-10-08 11:58:24 -0500

    update change Error

--- src/graphql/resolvers.ts
+++ src/graphql/resolvers.ts
@@ -1,8 +1,8 @@
 import Analisis, { getAllArticulos } from "../controllers/analisisArticulosC";
 const Resolvers = {
   Query: {
-   articulos: async () => await getAllArticulos(),
-   metadiaria: (obj: any, args: any, context: any, info: any) => Analisis(obj, args, context, info),
+    articulos: async () => await getAllArticulos(),
+    metadiaria: (obj: any, args: any, context: any, info: any) => Analisis(obj, args, context, info),
   },
 };
 
